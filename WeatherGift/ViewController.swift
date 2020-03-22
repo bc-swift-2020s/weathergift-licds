f53361b902124bac0bf2a92ef304f9569c546cd9
Yi Li <licds@bc.edu>
2020-03-22 16:21:54 -0400

    Initial Commit

---
+++ WeatherGift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherGift
+//
+//  Created by Yi Li on 3/22/20.
+//  Copyright © 2020 Yi Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
